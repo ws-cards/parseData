5ff38bb5bd444158e57ee58d1c0e42e3f1c51850
ws-cards <72481141+ws-cards@users.noreply.github.com>
2026-05-04 17:48:35 +0800

    Add files via upload

---
+++ src/main/java/ext/parse/parsedata/ParsedataApplication.java
@@ -0,0 +1,13 @@
+package ext.parse.parsedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParsedataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParsedataApplication.class, args);
+	}
+
+}
