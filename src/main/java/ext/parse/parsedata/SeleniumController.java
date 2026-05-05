5cd0ebb29a0732a448a181b6f678e4b7be451840
ws-cards <72481141+ws-cards@users.noreply.github.com>
2026-05-05 13:06:49 +0800

    Update SeleniumController.java

--- src/main/java/ext/parse/parsedata/SeleniumController.java
+++ src/main/java/ext/parse/parsedata/SeleniumController.java
@@ -12,7 +12,9 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "https://ws-cards.cloud")
 @RestController
 public class SeleniumController {
 
