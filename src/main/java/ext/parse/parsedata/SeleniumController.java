23ad70e11189fdd1d74b115047a014b56489f373
ws-cards <72481141+ws-cards@users.noreply.github.com>
2026-05-05 12:03:40 +0800

    Update SeleniumController.java

--- src/main/java/ext/parse/parsedata/SeleniumController.java
+++ src/main/java/ext/parse/parsedata/SeleniumController.java
@@ -9,11 +9,13 @@ import org.jsoup.select.Elements;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "https://ws-cards.cloud/")
 public class SeleniumController {
 
     @GetMapping("/health")
